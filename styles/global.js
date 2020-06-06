421db73995ee780f8ec2c23cbe26104c4af9756b
Pablo Freire <pablofreire6@gmail.com>
2020-06-06 01:16:52 -0300

    main screen

---
+++ styles/global.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: '2.5%',
+  },
+  button: {
+    backgroundColor: '#ff0000',
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
+
+export default globalStyles;
--- views/NewOrder.js
+++ views/NewOrder.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, StyleSheet, ImageBackground} from 'react-native';
+import {Container, Button, Text} from 'native-base';
+import globalStyles from '../styles/global';
+import {useNavigation} from '@react-navigation/native';
 
 const NewOrder = () => {
+  const navigation = useNavigation();
+
   return (
-    <>
-      <Text>New Order</Text>
-    </>
+    <Container style={[globalStyles.container, styles.container]}>
+      <ImageBackground
+        source={require('../images/bg_main.jpeg')}
+        style={styles.image}>
+        <View style={[globalStyles.content, styles.content]}>
+          <Text style={styles.heading}>Food App Concept</Text>
+          <Text style={styles.title}>Welcome!</Text>
+          <Text style={styles.subtitle}>
+            Login to your account or choose the city for being served without
+            login
+          </Text>
+
+          <Button
+            block
+            style={globalStyles.button}
+            onPress={() => navigation.navigate('Menu')}>
+            <Text style={globalStyles.buttonText}>Create a new order</Text>
+          </Button>
+        </View>
+      </ImageBackground>
+    </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#888',
+  },
+  content: {
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    marginBottom: 50,
+  },
+  title: {
+    fontSize: 45,
+    color: '#fff',
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#fff',
+    marginBottom: 30,
+  },
+  heading: {
+    color: '#fff',
+  },
+  image: {
+    resizeMode: 'cover',
+    flex: 1,
+  },
+});
 
 export default NewOrder;
